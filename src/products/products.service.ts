fea5f8f259b14119cfd6ffe5073fb9275ebd1d12
Marco Alvarado <marco.alvarado.t@gmail.com>
2024-06-14 01:56:01 -0400

    Project Set-up until 4.9

---
+++ src/products/products.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from '../common/dto/pagination.dto';
+import { last } from 'rxjs';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('ProductsService');
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Connected to the database');
+  }
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: createProductDto,
+    });
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+
+    const totalItems = await this.product.count();
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+      }),
+      metadata: {
+        totalItems,
+        currentPage: page,
+        lastPage: Math.ceil(totalItems / limit),
+      },
+    };
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
