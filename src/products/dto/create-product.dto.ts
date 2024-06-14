3d14a72f9ff8f49ac89c32ed1aace74f389a91ff
Marco Alvarado <marco.alvarado.t@gmail.com>
2024-06-14 02:57:32 -0400

    feat(ms-414): transform into a microservice

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,16 +1,12 @@
-import { Type } from "class-transformer";
-import { IsNumber, IsString, Min } from "class-validator";
+import { Type } from 'class-transformer';
+import { IsNumber, IsString, Min } from 'class-validator';
 
 export class CreateProductDto {
+  @IsString()
+  public name: string;
 
-    @IsString()
-    public name: string;
-    
-
-    @IsNumber({maxDecimalPlaces: 2})
-    @Min(0)
-    @Type(() => Number)
-    public price: number;
-
-
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @Min(0)
+  @Type(() => Number)
+  public price: number;
 }
